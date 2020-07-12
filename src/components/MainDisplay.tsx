2bd9bed70e1be4fa0fb15ee672fa83b236e7cf76
Dušan Andrić <Dusan.Andric@gmail.com>
2020-07-12 08:25:02 -0400

    re-orged main display

--- src/components/MainDisplay.tsx
+++ src/components/MainDisplay.tsx
@@ -6,10 +6,28 @@ export const MainDisplay = (props: any) => {
     return (
         <div className="ui grid">
             <div className="one column row">
-                <Camera/>
+                <h2 className="column block inverted section-header">Camera</h2>
             </div>
             <div className="one column row">
-                <QrCode/>
+                <div className="column center">
+                    <Camera/>
+                </div>
+            </div>
+
+            <div className="one column row">
+                <h2 className="column block inverted section-header">Environmentals</h2>
+            </div>
+            <div className="one column row">
+                <div className="column center section-text">
+                    UNDER CONSTRUCTION
+                </div>
+            </div>
+
+
+            <div className="one column center row">
+                <div className="column center">
+                    <QrCode/>
+                </div>
             </div>
         </div>
     );
--- src/components/QrCode.tsx
+++ src/components/QrCode.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
-export const QrCode = (props: any) => {
-    return (
-        <img src={`${process.env.REACT_APP_CAM_API}/qrcode.svg`} alt="qrcode" />
-    );
+type QrState = {
+    show: boolean
+}
+
+export class QrCode extends React.Component<any, QrState> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            show: false
+        };
+    }
+
+    render() {
+        if (this.state.show) {
+            return (
+                <img src={`${process.env.REACT_APP_CAM_API}/qrcode.svg`} alt="qrcode" />
+            );
+        } else {
+            return (
+                <button className="ui button" onClick={() => this.showCode()}>Show QRCode</button>
+            );
+        }
+    }
+
+    private showCode() {
+        this.setState({
+            show: true
+        });
+    }
+
 }
